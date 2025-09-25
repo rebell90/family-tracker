9a120c63581fe69c5027e574b44b694df2438420
Becca Bell <becelibel@gmail.com>
2025-09-24 21:23:35 -0500

    fix auth files

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -228,7 +228,7 @@ console.log('Debug info:', {
     )
   }
 
-  if (isParent && showFamilyManager) {
+  if (showFamilyManager) {
     return (
       <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-50 p-6">
         <div className="max-w-4xl mx-auto">
