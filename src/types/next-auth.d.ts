a3f67aa06a3d48ac9f8a0a9e5214b6b8fe8097b0
Becca Bell <becelibel@gmail.com>
2025-09-24 20:23:10 -0500

    fix auth files

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,33 @@
+// types/next-auth.d.ts
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      role?: string | null
+      familyId?: string | null
+    }
+  }
+
+  interface User {
+    id: string
+    name?: string | null
+    email?: string | null
+    image?: string | null
+    role?: string | null
+    familyId?: string | null
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    role?: string | null
+    familyId?: string | null
+  }
+}
+
