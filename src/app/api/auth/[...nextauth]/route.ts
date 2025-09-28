445ad3ae23f293719f900edae414a95c50cf7049
Becca Bell <becelibel@gmail.com>
2025-09-28 01:29:59 -0500

    config auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server"
 import NextAuth from "next-auth"
 import { authOptions } from "@/lib/auth"
 
-const handler = (req: NextRequest, ctx: any) => NextAuth(req, ctx, authOptions)
+const handler = (req: NextRequest, context: { params: { nextauth: string[] } }) => 
+  NextAuth(req, context, authOptions)
 
 export { handler as GET, handler as POST }
